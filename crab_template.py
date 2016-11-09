fb65b04807dd8e5b23037904524d51b2f66b7c52
Jia Fu Low <jia.fu.low@cern.ch>
2016-11-09 14:28:43 -0600

    Commit the psets

---
+++ crab_template.py
@@ -0,0 +1,41 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'XX-LABEL-XX'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+#config.JobType.pluginName = 'Analysis'
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'XX-CONFIG-XX'
+#config.JobType.outputFiles = ['Output.root']
+#config.JobType.pyCfgParams = []
+#config.JobType.maxMemoryMB = 3000
+
+#config.Data.inputDataset = 'XX-DATASET-XX'
+#config.Data.inputDBS = 'global'
+config.Data.outputPrimaryDataset = 'XX-DATASET-XX'
+#config.Data.splitting = 'FileBased'
+#config.Data.unitsPerJob = 1
+#config.Data.splitting = 'EventAwareLumiBased'
+#config.Data.unitsPerJob = 1000
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 100000
+njobs = XX-NJOBS-XX
+config.Data.totalUnits = config.Data.unitsPerJob * njobs
+config.Data.outLFNDirBase = '/store/group/l1upgrades/SLHC/GEN/620_SLHC28p1/XX-LABEL-XX/'
+config.Data.publication = False
+config.Data.outputDatasetTag = 'CRAB3'
+
+config.Site.storageSite = 'T3_US_FNALLPC'
+#config.Site.storageSite = 'T2_US_Florida'
+
+
+test_run = False
+if test_run:
+    #config.Data.splitting = 'EventAwareLumiBased'
+    config.Data.splitting = 'EventBased'
+    config.Data.unitsPerJob = 200
+    config.Data.totalUnits = config.Data.unitsPerJob * 1
+
---
+++ run_all_20161109.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Using CMSSW_BASE: $CMSSW_BASE"
+
+#./run.sh pset_SingleMuon_tt27_20161109.py ParticleGuns 2000
+./run.sh pset_SingleMuon_oc_tt25_20161109.py ParticleGuns 2000
