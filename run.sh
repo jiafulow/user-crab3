2c0c92accc49352fbb7a440337cd42384a72c275
Jia Fu Low <jia.fu.low@cern.ch>
2017-06-13 12:39:54 -0400

    More update

--- run.sh
+++ run.sh
@@ -9,7 +9,12 @@ echo "Run config=$config dataset=$dataset njobs=$njobs label=$label"
 
 mkdir -p crab_projects crab_projects_old
 rm -rf crab_projects_old/${projdir}
-[ -d crab_projects/${projdir} ] && mv crab_projects/${projdir} crab_projects_old
+
+if [ -d crab_projects/${projdir} ]; then
+  taskname=`grep 'Task name' crab_projects/${projdir}/crab.log | head -1 | sed -e 's/.* \([0-9]\+_[0-9]\+\):.*/\1/'`
+  mv crab_projects/${projdir} crab_projects_old/${projdir}_${taskname}
+fi
+
 sed "s@XX-LABEL-XX@$label@g" crab_template.py | sed "s@XX-DATASET-XX@$dataset@g" | sed "s@XX-CONFIG-XX@$config@g" | sed "s@XX-NJOBS-XX@$njobs@g"  > crab.py
 crab submit -c crab.py
 #crab submit -c crab.py --dryrun
