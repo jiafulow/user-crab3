1b544339c22672a9c14b7593fec7c753d062e651
Jia Fu Low <jia.fu.low@cern.ch>
2017-06-14 10:25:10 -0400

    Update to more update

--- crab_template.py
+++ crab_template.py
@@ -12,6 +12,7 @@ config.JobType.psetName = 'XX-CONFIG-XX'
 #config.JobType.outputFiles = ['Output.root']
 #config.JobType.pyCfgParams = []
 config.JobType.maxMemoryMB = 4000
+config.JobType.maxJobRuntimeMin = 1315*2
 config.JobType.numCores = 4
 
 #config.Data.inputDataset = 'XX-DATASET-XX'
@@ -26,12 +27,13 @@ config.Data.unitsPerJob = 1000  # for ParticleGuns
 #config.Data.unitsPerJob = 10000  # for MinBias
 njobs = XX-NJOBS-XX
 config.Data.totalUnits = config.Data.unitsPerJob * njobs
-config.Data.outLFNDirBase = '/store/user/jiafulow/L1MuonTrigger/P2_9_2_0/XX-LABEL-XX/'
+#config.Data.outLFNDirBase = '/store/user/jiafulow/L1MuonTrigger/P2_9_2_0/XX-LABEL-XX/'
+config.Data.outLFNDirBase = '/store/group/l1upgrades/L1MuonTrigger/P2_9_2_0/XX-LABEL-XX/'
 config.Data.publication = False
 config.Data.outputDatasetTag = 'CRAB3'
 
-#config.Site.storageSite = 'T3_US_FNALLPC'
-config.Site.storageSite = 'T2_US_Florida'
+#config.Site.storageSite = 'T2_US_Florida'
+config.Site.storageSite = 'T3_US_FNALLPC'
 
 
 test_run = False
