0fb7aa17443910c996805503a9b215704847e753
Jia Fu Low <jia.fu.low@cern.ch>
2017-06-28 02:14:41 -0400

    Update. Working on generating PU140 samples

--- run_all_20170609.sh
+++ run_all_20170609.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Using CMSSW_BASE: $CMSSW_BASE"
+echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 
 ./run.sh pset_SingleMuon_PositiveEndCap_20170609.py                ParticleGuns 200
 
