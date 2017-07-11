3f753cc9072fdb15120b77bd50e8c9fdfa5c578b
Jia Fu Low <jia.fu.low@cern.ch>
2017-07-10 20:37:08 -0400

    Resubmit MinBias task, now going for 20M stats

--- run_all_20170613.sh
+++ run_all_20170613.sh
@@ -2,9 +2,9 @@
 
 echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 
-#./run.sh pset_SingleMuon_PositiveEndCap_20170613.py                ParticleGuns 200
+#./run.sh pset_MinBias_TuneCUETP8M1_14TeV-pythia8_20170613.py       MinBias      2000
 
-#./run.sh pset_MinBias_TuneCUETP8M1_14TeV-pythia8_20170613.py       MinBias      200
+#./run.sh pset_SingleMuon_PositiveEndCap_20170613.py                ParticleGuns 200
 
 #./run.sh pset_SingleNeutrino_PU50_20170613.py                       ParticleGuns 200
 #./run.sh pset_SingleNeutrino_PU100_20170613.py                      ParticleGuns 200
