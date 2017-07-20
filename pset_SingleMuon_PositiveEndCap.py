f08deabc66b11c57db9ed623ed3535f3a722f698
Jia Fu Low <jia.fu.low@cern.ch>
2017-07-20 19:24:39 -0400

    Add ntuple psets
    Add run.ntuple.sh
    Add run_all_20170720.sh

---
+++ pset_SingleMuon_PositiveEndCap.py
@@ -0,0 +1,1 @@
+/home/jlow/L1MuonTrigger/P2_CMSSW_9_2_3_patch1/src/L1TMuonSimulations/Analyzers/test2/pset_SingleMuon_PositiveEndCap.py
---
+++ run_all_20170720.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
+
+#./run.ntuple.sh ntuple_SingleMuon_PositiveEndCap_20170720.py  ParticleGuns -1
+./run.ntuple.sh ntuple_SingleNeutrino_PU140_20170720.py       ParticleGuns -1
+
