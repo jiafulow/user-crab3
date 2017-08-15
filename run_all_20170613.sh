5e780e3e2380d3f30b686db687065bfc803ca01a
Jia Fu Low <jia.fu.low@cern.ch>
2017-08-15 19:32:26 -0400

    Add pset_SingleNeutrino_PU15_20170613.py

--- run_all_20170613.sh
+++ run_all_20170613.sh
@@ -13,7 +13,8 @@ echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 #./run.sh pset_SingleNeutrino_PU200_20170613.py                      ParticleGuns 2000
 #./run.sh pset_SingleNeutrino_r281707_20170613.py                    ParticleGuns 2000
 #./run.sh pset_SingleNeutrino_r281707_run2_20170613.py               ParticleGuns 2000
-./run.sh pset_SingleNeutrino_PU0_20170613.py                        ParticleGuns 2000
+#./run.sh pset_SingleNeutrino_PU0_20170613.py                        ParticleGuns 2000
+./run.sh pset_SingleNeutrino_PU15_20170613.py                       ParticleGuns 2000
 
 #./run.sh pset_SingleMuon_PositiveEndCap_PU140_20170613.py           ParticleGuns 2000
 #./run.sh pset_SingleMuon_PositiveEndCap_PU200_20170613.py           ParticleGuns 2000
