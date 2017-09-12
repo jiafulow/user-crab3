673cdfbc557349e9d40e0657e0954215903fad2d
Jia Fu Low <jia.fu.low@cern.ch>
2017-09-12 15:20:24 -0400

    Update ntuple_*.py

--- run_all_20170720.sh
+++ run_all_20170720.sh
@@ -4,9 +4,6 @@ echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 
 #./run.ntuple.sh ntuple_SingleMuon_PositiveEndCap_20170720.py  ParticleGuns -1
 
-#./run.ntuple.sh ntuple_SingleNeutrino_r281707_20170720.py     ParticleGuns -1
-#./run.ntuple.sh ntuple_SingleNeutrino_PU50_20170720.py        ParticleGuns -1
-#./run.ntuple.sh ntuple_SingleNeutrino_PU100_20170720.py       ParticleGuns -1
 #./run.ntuple.sh ntuple_SingleNeutrino_PU140_20170720.py       ParticleGuns -1
 #./run.ntuple.sh ntuple_SingleNeutrino_PU200_20170720.py       ParticleGuns -1
 
