8cbea8219b93819475dfc9b29dcf48c3e505a57e
Jia Fu Low <jia.fu.low@cern.ch>
2017-10-05 06:19:59 -0400

    Add pset_SingleMuon_Toy_*.py
    Add run_all_20171004.sh

---
+++ run_all_20171004.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
+
+./run.sh pset_SingleMuon_Toy_20171004.py                       ParticleGuns 100
+./run.sh pset_SingleMuon_Toy_0T_20171004.py                    ParticleGuns 100
+./run.sh pset_SingleMuon_Toy_0M_20171004.py                    ParticleGuns 100
