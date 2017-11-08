3d33e98992216bba8c91cbc221534a1c8f1f9338
Jia Fu Low <jia.fu.low@cern.ch>
2017-11-08 10:53:53 -0500

    Add rateplots_mc_SingleNeutrino_r305310_run2_20170725.py

--- run_all_20170725.sh
+++ run_all_20170725.sh
@@ -5,6 +5,7 @@ echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 #./run.ntuple-SingleMuonPD.sh rateplots_data_SingleMuonPD_r281707_run2_20170725.py SingleMuonPD -1
 
 #./run.ntuple.sh rateplots_mc_SingleNeutrino_r281707_run2_20170725.py              ParticleGuns -1
+#./run.ntuple.sh rateplots_mc_SingleNeutrino_r305310_run2_20170725.py              ParticleGuns -1
 ./run.ntuple.sh rateplots_mc_SingleNeutrino_r281707_20170725.py                   ParticleGuns -1
 #./run.ntuple.sh rateplots_mc_SingleNeutrino_PU0_20170725.py                       ParticleGuns -1
 ./run.ntuple.sh rateplots_mc_SingleNeutrino_PU15_20170725.py                      ParticleGuns -1
