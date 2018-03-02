58d2379bb1d6cdb6da1b789225bcfef58b2578c2
Jia Fu Low <jia.fu.low@cern.ch>
2018-03-02 17:37:06 -0500

    Update

--- run_all_20171004.sh
+++ run_all_20171004.sh
@@ -6,3 +6,6 @@ echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 ./run.sh pset_SingleMuon_Toy_0T_20171004.py                    ParticleGuns 100
 ./run.sh pset_SingleMuon_Toy_0M_20171004.py                    ParticleGuns 100
 ./run.sh pset_SingleMuon_Toy_5GeV_20171004.py                  ParticleGuns 2000
+./run.sh pset_SingleMuon_Toy_3GeV_20171004.py                  ParticleGuns 2000
+./run.sh pset_SingleMuon_Toy_2GeV_20171004.py                  ParticleGuns 2000
+./run.sh pset_SingleMuon_Toy2_2GeV_20171004.py                 ParticleGuns 2000
