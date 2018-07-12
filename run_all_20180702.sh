7e905590f4fb8d8b1a7abac44f30e8f1703ea1f8
Jia Fu Low <jia.fu.low@cern.ch>
2018-07-12 16:18:24 -0400

    Disable multithreading

--- run_all_20180702.sh
+++ run_all_20180702.sh
@@ -2,5 +2,5 @@
 
 echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 
-./run.sh pset_SingleMuon_Toy_2GeV_20180702.py                  ParticleGuns 500
-./run.sh pset_SingleMuon_Toy2_2GeV_20180702.py                 ParticleGuns 500
+./run.sh pset_SingleMuon_Toy_2GeV_20180702.py                  ParticleGuns 2000
+./run.sh pset_SingleMuon_Toy2_2GeV_20180702.py                 ParticleGuns 2000
