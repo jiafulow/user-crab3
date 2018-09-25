41c1d7879e985f2b3e9676465342c1a0b6d29864
Jia Fu Low <jia.fu.low@cern.ch>
2018-09-24 21:11:28 -0400

    Add PU140, PU200

--- run_all_20180704.sh
+++ run_all_20180704.sh
@@ -2,4 +2,11 @@
 
 echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 
+./run.ntuple.sh ntuple_SingleNeutrino_PU140_20180704.py       ParticleGuns -1
 ./run.ntuple.sh ntuple_SingleNeutrino_PU200_20180704.py       ParticleGuns -1
+./run.ntuple.sh ntuple_SingleMuon_PU140_20180704.py           ParticleGuns -1
+./run.ntuple.sh ntuple_SingleMuon_PU200_20180704.py           ParticleGuns -1
+./run.ntuple.sh ntuple_SingleElectron_PU140_20180704.py       ParticleGuns -1
+./run.ntuple.sh ntuple_SingleElectron_PU200_20180704.py       ParticleGuns -1
+./run.ntuple.sh ntuple_SinglePhoton_PU140_20180704.py         ParticleGuns -1
+./run.ntuple.sh ntuple_SinglePhoton_PU200_20180704.py         ParticleGuns -1
