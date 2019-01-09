c380cdd24f6751fc38dfa4223ac139f660181b8d
Jia Fu Low <jia.fu.low@cern.ch>
2019-01-08 21:58:48 -0500

    Commit psets

---
+++ run_all_20190108.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
+
+./run.sh pset_SingleMuon_Overlap_2GeV_20190108.py                  ParticleGuns 2000
+./run.sh pset_SingleMuon_Overlap2_2GeV_20190108.py                 ParticleGuns 2000
