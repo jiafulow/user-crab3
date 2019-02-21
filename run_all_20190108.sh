06e4797a6aa2cb711568e322cdff1cf2d688ffd8
Jia Fu Low <jia.fu.low@cern.ch>
2019-02-21 01:13:29 -0500

    Add psets

--- run_all_20190108.sh
+++ run_all_20190108.sh
@@ -2,8 +2,11 @@
 
 echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 
+./run.sh pset_SingleMuon_Endcap_2GeV_20190108.py                   ParticleGuns 2000
+./run.sh pset_SingleMuon_Endcap2_2GeV_20190108.py                  ParticleGuns 2000
+
 ./run.sh pset_SingleMuon_Overlap_3GeV_20190108.py                  ParticleGuns 1000
 ./run.sh pset_SingleMuon_Overlap2_3GeV_20190108.py                 ParticleGuns 1000
 
-./run.sh pset_SingleMuon_Endcap_2GeV_20190108.py                   ParticleGuns 2000
-./run.sh pset_SingleMuon_Endcap2_2GeV_20190108.py                  ParticleGuns 2000
+./run.sh pset_SingleMuon_Displaced_2GeV_20190108.py                ParticleGuns 1000
+./run.sh pset_SingleMuon_Displaced2_2GeV_20190108.py               ParticleGuns 1000
