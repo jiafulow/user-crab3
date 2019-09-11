7ada2f34356479cdcf62ce744789c68c03cbcab6
Jia Fu Low <jia.fu.low@cern.ch>
2019-09-11 19:03:25 -0400

    Add psets

--- run_all_20190906.sh
+++ run_all_20190906.sh
@@ -11,6 +11,9 @@ echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 
 ## Running over published datasets
 #./run.mc.sh ntuple_MuMu_FlatPt_PU0_PhaseIITDRSpring19_20190906.py          /Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-NoPU_106X_upgrade2023_realistic_v3-v1/GEN-SIM-DIGI-RAW  -1
-./run.mc.sh ntuple_MuMu_FlatPt_PU200_PhaseIITDRSpring19_20190906.py        /Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v2/GEN-SIM-DIGI-RAW -1
-./run.mc.sh ntuple_SingleNeutrino_PU200_PhaseIITDRSpring19_20190906.py     /Nu_E10-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v3/GEN-SIM-DIGI-RAW          -1
-./run.mc.sh ntuple_SingleNeutrino_PU200_PhaseIIMTDTDRAutumn18_20190906.py  /NeutrinoGun_E_10GeV/PhaseIIMTDTDRAutumn18DR-PU200_103X_upgrade2023_realistic_v2-v1/FEVT                  -1
+#./run.mc.sh ntuple_MuMu_FlatPt_PU200_PhaseIITDRSpring19_20190906.py        /Mu_FlatPt2to100-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v2/GEN-SIM-DIGI-RAW -1
+#./run.mc.sh ntuple_SingleNeutrino_PU200_PhaseIITDRSpring19_20190906.py     /Nu_E10-pythia8-gun/PhaseIITDRSpring19DR-PU200_106X_upgrade2023_realistic_v3-v3/GEN-SIM-DIGI-RAW          -1
+#./run.mc.sh ntuple_SingleNeutrino_PU200_PhaseIIMTDTDRAutumn18_20190906.py  /NeutrinoGun_E_10GeV/PhaseIIMTDTDRAutumn18DR-PU200_103X_upgrade2023_realistic_v2-v1/FEVT                  -1
+
+./run.mc.sh ntuple_SingleNeutrino_PU140_PhaseIITDRSpring19_20190906.py     /Nu_E10-pythia8-gun/PhaseIITDRSpring19DR-PU140_106X_upgrade2023_realistic_v3_ext3-v1/GEN-SIM-DIGI-RAW     -1
+./run.mc.sh ntuple_SingleNeutrino_PU250_PhaseIITDRSpring19_20190906.py     /Nu_E10-pythia8-gun/PhaseIITDRSpring19DR-PU250_106X_upgrade2023_realistic_v3_ext2-v1/GEN-SIM-DIGI-RAW     -1
