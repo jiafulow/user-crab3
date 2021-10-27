62bfd50fea30862e4f3db711c81676b79616ed64
Jia Fu Low <jia.fu.low@cern.ch>
2021-10-26 23:20:12 -0400

    Add psets

---
+++ p/pset_SingleMuon_PosEnd_FlatVtx_2GeV_Phase2HLTTDRSummer20_20210724.py
@@ -0,0 +1,180 @@
+# Auto generated configuration file
+# using:
+# Revision: 1.19
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v
+# with command line options: L1TMuonSimulations/Configuration/python/SingleMuon_PosEnd_2GeV_cfi.py -n 100 -s GEN,SIM,DIGI:pdigi_valid,L1TrackTrigger,L1 --conditions auto:phase2_realistic --era Phase2C9 --no_exec --fileout file:SingleMuon_Endcap.root --eventcontent FEVTDEBUGHLT --pileup NoPileUp --beamspot HLLHC14TeV --datatier GEN-SIM-DIGI-RAW --python_filename pset_SingleMuon_PosEnd_2GeV_Phase2HLTTDRSummer20.py --customise SimGeneral/MixingModule/customiseStoredTPConfig.higherPtTP,SLHCUpgradeSimulations/Configuration/aging.customise_aging_1000,L1Trigger/Configuration/customisePhase2TTOn110.customisePhase2TTOn110,L1TMuonSimulations/Configuration/customise.customise_ntupler_step --geometry Extended2026D49 --mc
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Phase2C9_cff import Phase2C9
+
+process = cms.Process('L1',Phase2C9)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2026D49Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D49_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+#process.load('IOMC.EventVertexGenerators.VtxSmearedHLLHC14TeV_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.L1TrackTrigger_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+    FailPath = cms.untracked.vstring(),
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    SkipEvent = cms.untracked.vstring(),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(1)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('L1TMuonSimulations/Configuration/python/SingleMuon_PosEnd_2GeV_cfi.py nevts:100'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-DIGI-RAW'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:SingleMuon_Endcap.root'),
+    outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+process.mix.digitizers = cms.PSet(process.theDigitizersValid)
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
+
+process.generator = cms.EDProducer("FlatRandomPtGunProducer2",
+    AddAntiParticle = cms.bool(False),
+    PGunParameters = cms.PSet(
+        MaxEta = cms.double(2.8),
+        MaxPhi = cms.double(3.14159265359),
+        MaxPt = cms.double(7000.0),
+        MinEta = cms.double(1.1),
+        MinPhi = cms.double(-3.14159265359),
+        MinPt = cms.double(2.0),
+        PartID = cms.vint32(-13),
+        PtSpectrum = cms.string('flatOneOverPt'),
+        RandomCharge = cms.bool(True)
+    ),
+    Verbosity = cms.untracked.int32(0),
+    firstRun = cms.untracked.uint32(1),
+    psethack = cms.string('single muon+/- pt 2 to 7000 flat in 1/pt positive endcap')
+)
+
+# Modify process.VtxSmeared
+process.load('L1TMuonSimulations.Configuration.VtxSmearedFlatVtx_cfi')
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi_valid)
+process.L1TrackTrigger_step = cms.Path(process.L1TrackTrigger)
+process.pL1TkPrimaryVertex = cms.Path(process.L1TkPrimaryVertex)
+process.pL1TkPhotonsCrystal = cms.Path(process.L1TkPhotonsCrystal)
+process.pL1TkIsoElectronsCrystal = cms.Path(process.L1TkIsoElectronsCrystal)
+process.pL1TkElectronsLooseCrystal = cms.Path(process.L1TkElectronsLooseCrystal)
+process.pL1TkElectronsHGC = cms.Path(process.L1TkElectronsHGC)
+process.pL1TkMuon = cms.Path(process.L1TkMuons+process.L1TkMuonsTP)
+process.pL1TkElectronsLooseHGC = cms.Path(process.L1TkElectronsLooseHGC)
+process.pL1TkElectronsEllipticMatchHGC = cms.Path(process.L1TkElectronsEllipticMatchHGC)
+process.pL1TkElectronsCrystal = cms.Path(process.L1TkElectronsCrystal)
+process.pL1TkPhotonsHGC = cms.Path(process.L1TkPhotonsHGC)
+process.pL1TkIsoElectronsHGC = cms.Path(process.L1TkIsoElectronsHGC)
+process.pL1TkElectronsEllipticMatchCrystal = cms.Path(process.L1TkElectronsEllipticMatchCrystal)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.digitisation_step,process.L1TrackTrigger_step,process.L1simulation_step,process.endjob_step,process.FEVTDEBUGHLToutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+# filter all path with the production filter sequence
+for path in process.paths:
+    getattr(process,path).insert(0, process.generator)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from SimGeneral.MixingModule.customiseStoredTPConfig
+from SimGeneral.MixingModule.customiseStoredTPConfig import higherPtTP
+
+# call to customisation function higherPtTP imported from SimGeneral.MixingModule.customiseStoredTPConfig
+process = higherPtTP(process)
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.aging
+from SLHCUpgradeSimulations.Configuration.aging import customise_aging_1000
+
+# call to customisation function customise_aging_1000 imported from SLHCUpgradeSimulations.Configuration.aging
+process = customise_aging_1000(process)
+
+# Automatic addition of the customisation function from L1Trigger.Configuration.customisePhase2TTOn110
+from L1Trigger.Configuration.customisePhase2TTOn110 import customisePhase2TTOn110
+
+# call to customisation function customisePhase2TTOn110 imported from L1Trigger.Configuration.customisePhase2TTOn110
+process = customisePhase2TTOn110(process)
+
+# Automatic addition of the customisation function from L1TMuonSimulations.Configuration.customise
+from L1TMuonSimulations.Configuration.customise import customise_ntupler_step
+
+# call to customisation function customise_ntupler_step imported from L1TMuonSimulations.Configuration.customise
+process = customise_ntupler_step(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
--- run_all_20210724.sh
+++ run_all_20210724.sh
@@ -6,6 +6,11 @@ echo "[INFO] Using CMSSW_BASE: $CMSSW_BASE"
 ./run.sh p/pset_SingleMuon_PosEnd_2GeV_Phase2HLTTDRSummer20_20210724.py ParticleGuns 1000
 ./run.sh p/pset_SingleMuon_NegEnd_2GeV_Phase2HLTTDRSummer20_20210724.py ParticleGuns 1000
 
+./run.sh p/pset_SingleMuon_PosEnd_FlatD0_2GeV_Phase2HLTTDRSummer20_20210724.py ParticleGuns 1000
+./run.sh p/pset_SingleMuon_NegEnd_FlatD0_2GeV_Phase2HLTTDRSummer20_20210724.py ParticleGuns 1000
+./run.sh p/pset_SingleMuon_PosEnd_FlatVtx_2GeV_Phase2HLTTDRSummer20_20210724.py ParticleGuns 1000
+./run.sh p/pset_SingleMuon_NegEnd_FlatVtx_2GeV_Phase2HLTTDRSummer20_20210724.py ParticleGuns 1000
+
 # Run on published datasets
 # Note: last argument is used for unitsPerJob (how many files per job), instead of njobs
 ./run.mc.sh p/pset_SingleNeutrino_PU200_Phase2HLTTDRSummer20_20210724.py /MinBias_TuneCP5_14TeV-pythia8/Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1/FEVT 40
